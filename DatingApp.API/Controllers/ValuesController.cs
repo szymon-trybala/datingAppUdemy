fe47444f2348321dd9577e4877bd42717f961125
Szymon <szymon.trybala@protonmail.com>
2019-05-11 02:22:10 +0200

    Done about half of security section(lessons 23-30)

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -33,7 +33,7 @@ namespace DatingApp.API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
         // POST api/values
