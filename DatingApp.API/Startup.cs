99a199ac95c1391248b39964976926ba85d628c9
Szymon <szymon.trybala@protonmail.com>
2019-05-17 02:21:16 +0200

    1/2 of S8, extender user model and seeded database

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -37,6 +37,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
                 options.TokenValidationParameters = new TokenValidationParameters 
@@ -50,7 +51,7 @@ namespace DatingApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -77,6 +78,8 @@ namespace DatingApp.API
             // OGARNĄĆ POTEM - .net core web api ma tryb developement i produkcyjny, ten drugi korzysta z hsts który przetwarza na https
             // app.UseHttpsRedirection();
             
+            // Metoda do stworzenia userow w bazie danych, uzyc jesli sie ja usunelo
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             // Uruchamia mvc dzięki któremu requesty są przekierowane do odpowiedniego kontrolera
